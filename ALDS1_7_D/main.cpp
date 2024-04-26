9282db39f15f15d7bfdce561068a59cfb8136595
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-26 10:59:09 +0900

    init 1 7 d and move files

---
+++ ALDS1_7_D/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll  = long long;
+using ld  = long double;
+using pll = pair<ll, ll>;
+#define rep(i, n)       for (ll i = 0; i < n; ++i)
+#define all(c)          begin(c), end(c)
+#define PI              acos(-1)
+#define oo              2e18
+#define cinp(type, var) type var = c_input<type>()
+template<typename T1, typename T2>
+bool chmax(T1& a, T2 b) {
+  if (a < b) {
+    a = b;
+    return true;
+  } else
+    return false;
+}
+template<typename T1, typename T2>
+bool chmin(T1& a, T2 b) {
+  if (a > b) {
+    a = b;
+    return true;
+  } else
+    return false;
+}
+template<typename T>
+T c_input() {
+  T input;
+  cin >> input;
+  return input;
+}
+// priority_queue<ll, vector<ll>, greater<ll>> Q;
+// LMAX = 18446744073709551615 (1.8*10^19)
+// IMAX = 2147483647 (2.1*10^9)
+/*
+
+*/
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(10);
+
+
+}
+
