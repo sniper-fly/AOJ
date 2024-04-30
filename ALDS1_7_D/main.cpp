30ce5374f845e0b819bfaab7406ff8694469efb5
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-30 11:12:52 +0900

    木の再現、再帰を考える前に手続き的にコードを書く

--- ALDS1_7_D/main.cpp
+++ ALDS1_7_D/main.cpp
@@ -36,12 +36,50 @@ T c_input() {
 /*
 
 */
+struct Node
+{
+  int left;
+  int right;
+  int parent;
+};
+
+constexpr int NIL = -1;
 
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(0);
   cout << fixed << setprecision(10);
 
+  const cinp(int, len);
+  vector<int> preorder;
+  rep(i, len) {
+    const cinp(int, id);
+    preorder.push_back(id);
+  }
 
+  vector<int> inorder;
+  rep(i, len) {
+    const cinp(int, id);
+    inorder.push_back(id);
+  }
+
+
+  map<int, Node> tree;
+  // 3 2 5 4 6 1 8 7 9 を例に考える
+  int       i   = 0;
+  const int pre = preorder[i];
+  // pre を inorder 内から探して、preを根として左部分木と右部分木に分ける
+  const auto current_root_itr = find(inorder.begin(), inorder.end(), pre);
+  const int  id               = *current_root_itr;
+  if (tree.find(id) == tree.end()) tree[id].parent = NIL;
+
+  auto left_end    = current_root_itr - 1;
+  auto right_start = current_root_itr + 1;
+
+  auto next_root_itr = find(inorder.begin(), left_end, preorder[i + 1]);
+  left_end           = next_root_itr - 1;
+  right_start        = next_root_itr + 1;
+
+  next_root_itr = find(inorder.begin(), left_end, preorder[i + 2]);
+  //
 }
-
